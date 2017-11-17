2340c7b2d42e4dbc60dd6f9514b38555a29e2003
Blake Guilloud <blake.guilloud@gmail.com>
2017-11-16 23:38:07 -0500

    moar test

--- test/index.test.js
+++ test/index.test.js
@@ -41,7 +41,7 @@ describe('mergeShallow', () => {
     expect(sut[1].gender).toBe('male');
   });
 
-  it('should persist overwrite nested objects and arrays', () => {
+  it('should overwrite nested objects and arrays', () => {
     const obj1 = {
       1: {
         firstName: 'blake',
@@ -68,5 +68,74 @@ describe('mergeShallow', () => {
     expect(sut[1].pets[0]).toBe('kylo');
     expect(sut[1].skills.node).toBe(true);
     expect(sut[1].skills.react).toBe(undefined);
+  });
+
+  it('should preform complex merges', () => {
+    const obj1 = {
+      1: {
+        firstName: 'blake',
+        pets: ['kylo'],
+        skills: {
+          react: false,
+        },
+        parents: {
+          mark: {
+            type: 'father',
+            firstName: 'mark',
+            lastName: 'guilloud',
+          },
+          alison: {
+            type: 'mother',
+            firstName: 'alison',
+            lastName: 'guilloud',
+            parents: {
+              bobbie: {
+                firstName: 'barbara',
+                lastName: 'bailey',
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const obj2 = {
+      1: {
+        lastName: 'guilloud',
+        skills: {
+          node: true,
+        },
+        pets: ['kylo', 'leia'],
+        parents: {
+          alison: {
+            type: 'mother',
+            firstName: 'alison',
+            lastName: 'bailey',
+            parents: {
+              barbara: {
+                firstName: 'barbara',
+                lastName: 'bailey',
+              },
+              ray: {
+                firstName: 'ray',
+                lastName: 'bailey',
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const sut = mergeShallow(obj1, obj2);
+
+    expect(sut[1].firstName).toBe('blake');
+    expect(sut[1].lastName).toBe('guilloud');
+    expect(sut[1].pets[0]).toBe('kylo');
+    expect(sut[1].pets[1]).toBe('leia');
+    expect(sut[1].skills.node).toBe(true);
+    expect(sut[1].skills.react).toBe(undefined);
+    expect(sut[1].parents.mark).toBe(undefined);
+    expect(sut[1].parents.alison.lastName).toBe('bailey');
+    expect(sut[1].parents.alison.parents.ray.lastName).toBe('bailey');
   });
 });
